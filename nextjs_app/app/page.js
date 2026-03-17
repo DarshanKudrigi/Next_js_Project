be00d78c28d196fe22edf2fe82596a1ac2a88fff
DarshanKudrigi <darshankudrigi2005@gmail.com>
2026-03-17 17:42:26 +0530

    next.js

---
+++ nextjs_app/app/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+function darshan() {
+  return (
+    <div>
+      <h3>Welcome to the Home Page!</h3>
+      <p>This is a simple Next.js application.</p>
+    </div>
+  );
+}
+
+export default darshan;
