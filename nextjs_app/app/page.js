c70f254e4b34211412def36b430d219a5b717b17
DarshanKudrigi <darshankudrigi2005@gmail.com>
2026-03-18 23:37:27 +0530

    daily

--- nextjs_app/app/page.js
+++ nextjs_app/app/page.js
@@ -2,9 +2,11 @@ import React from "react";
 function darshan() {
   return (
     <div>
-      <h3>Welcome to the Home Page!</h3>
+      <h1>Welcome to the Home Page!</h1>
       <p>This is a simple Next.js application.</p>
       <h1>my name is Darshan</h1>
+      <h4>i am a software developer</h4>
+      <h2>i am learning next js</h2>
     </div>
   );
 }
