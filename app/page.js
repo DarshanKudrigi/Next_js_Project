d88f3cc6eb7e79700bfb8ec20cf3518bdfebf620
DarshanKudrigi <darshankudrigi2005@gmail.com>
2026-03-19 23:43:53 +0530

    about and contact

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+function darshan() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      <p>This is a simple Next.js application.</p>
+      <h1>my name is Darshan</h1>
+      <h4>i am a software developer</h4>
+      <h2>i am learning next js</h2>
+    </div>
+  );
+}
+
+export default darshan;
